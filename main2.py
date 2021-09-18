345b7790263d02fd139a6312fb3c17c3747935cb
omarhashem123 <70245773+omarhashem123@users.noreply.github.com>
2021-09-18 19:30:54 +0200

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,228 @@
+import argparse
+import os
+import concurrent.futures
+import re
+import subprocess
+import time
+
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('-o', help='output folder')
+parser.add_argument('-m', help='file contain scope all *.example.com(medium)  and with this example.com(small)')
+parser.add_argument('-cn', help='name of company on github')
+parser.add_argument('-ct', help='dns canary token for testing ssrf and os (oob)')
+parser.add_argument('-c', help='cookie')
+args = parser.parse_args()
+
+#logo
+os.system("python3 logo.py")
+time.sleep(5)
+#make dir for output
+os.system('mkdir ' + args.o)
+#[+]---------------------------------   medium and small recon   ----------------------------------------[+]
+scope_first = set(open(args.m).readlines())
+scope_medium = set()
+scope_small = set()
+for i in scope_first:
+    if '*' in i:
+        scope_medium.add(i.replace('*.', '').strip())
+    else:
+        scope_small.add(i.strip())
+
+                  #[+]-----------   passive and active subdomains enumeration   ------------[+]
+os.system("mkdir "+ args.o + "/recon")
+os.system("mkdir "+ args.o + "/recon/subdomains")
+file_for_scope = open(args.o + "/recon/subdomains/subdomains", "a")
+for i in scope_medium:
+	file_for_scope.write(i+"\n")
+	print(111)
+file_for_scope.close()
+os.system("subfinder -dL " + args.o + "/recon/subdomains/subdomains -o " + args.o + "/recon/subdomains/live_subdomains")
+#[+]----------------------------------------------------------------------------[+]
+
+#use httpx to extract unique live subdomains to enter it to scanners and wayback and dirsearch
+os.system("httpx -l " + args.o + "/recon/subdomains/live_subdomains -o " + args.o + "/recon/subdomains/httpx_subs")
+
+#[+]----------------------------------------------------------------------------[+]
+# def function needed in next concurrent.futures
+def dirsearch(sub):
+    os.system("python3 webtool/dirsearch/dirsearch.py --full-url --random-agent -e '*' -u " + sub)
+
+def wappalyzer(sub):
+    os.system("node webtool/technology/wappalyzer/src/drivers/npm/cli.js " + sub + " >> " + args.o + "/recon/technology/technologies.json")
+
+def GitDorker(nothing):
+    os.system("mkdir " + args.o + "/github_leaks")
+    os.system("python3 webtool/GitDorker/GitDorker.py -org " + args.cn + " -d webtool/GitDorker/Dorks/alldorksv3 -ri -tf webtool/GitDorker/tokens -lb -o " + args.o + "/github_leaks/github_leaks")
+
+def naabu(nothing):
+    os.system("naabu -exclude-cdn -iL " + args.o + "/recon/subdomains/live_subdomains -p 1-65535 --rate 20000 -nmap-cli 'nmap -sV -oA " + args.o + "/network/nmap'")
+
+def subover(nothing):
+    os.system("SubOver -l " + args.o + "/recon/subdomains/live_subdomains -t 100 > " + args.o + "/subdomain_takeover/subover")
+
+def wayback(nothing):
+    os.system("cat " + args.o + "/recon/subdomains/httpx_subs |sed 's+http://++gi'|sed 's+https://++gi'|sort -u|gau >> " + args.o + "/recon/all_endpoints_and_gf/endpoints")
+def clickjacking(nothing):
+    os.system("mkdir " + args.o + "/other/clickjacking")
+    os.system("python3 webtool/clickjack/clickjack.py -t 10 -i " + args.o + "/recon/subdomains/httpx_subs -o " + args.o + "/other/clickjacking/clickjacking_subs")
+def smuggler(sub):
+    os.system("python3 webtool/smuggler/smuggler.py -u " + sub + " -x -t 15")
+def s3brute(sub):
+    os.system("python webtool/s3brute/amazon-s3-enum.py -w webtool/s3brute/BucketNames.txt -d " + sub)
+def getjs(nothing):
+    os.system("mkdir " + args.o + "/js")
+    os.system("mkdir " + args.o + "/js/js-files-content")
+    os.system("cat "  + args.o + "/recon/subdomains/httpx_subs|subjs -t 30 >> " + args.o + "/js/js_files_links")
+    os.system("wget -i " + args.o + "/js/js_files_links -P " + args.o + "/js/js-files-content/")
+
+#start network scan and content discovery and some law hanging fruits
+#[+]----------------------------------------------------------------------------[+]
+os.system("mkdir " + args.o + "/network")
+os.system("mkdir " + args.o + "/other")
+os.system("mkdir " + args.o + "/other/smuggler")
+os.system("mkdir " + args.o + "/other/s3-bucket")
+os.system("mkdir " + args.o + "/other/dirsearch")
+os.system("mkdir " + args.o + "/subdomain_takeover")
+os.system("mkdir " + args.o + "/recon/technology")
+os.system("mkdir " + args.o + "/recon/all_endpoints_and_gf")
+os.system("mkdir " + args.o + "/recon/all_endpoints_and_gf")
+with concurrent.futures.ThreadPoolExecutor(max_workers=10) as e:
+    scope_dirsearch = set()
+    for i in open(args.o + "/recon/subdomains/httpx_subs"):
+        scope_dirsearch.add(i.strip())
+    scope_wappalyzer = set()
+    for i in open(args.o + "/recon/subdomains/httpx_subs"):
+        scope_wappalyzer.add(i.strip())
+    scope_s3brute = set()
+    for i in open(args.o + "/recon/subdomains/httpx_subs"):
+        i = i.replace("https://", "")
+        i = i.replace("http://", "")
+        scope_s3brute.add(i.strip())
+
+    nothing = [1]
+    #e.map(naabu, nothing)
+    e.map(subover, nothing)
+    e.map(dirsearch, scope_dirsearch)
+    #e.map(wappalyzer, scope_wappalyzer)
+    #e.map(GitDorker, nothing)
+    e.map(wayback, nothing)
+    e.map(clickjacking, nothing)
+    #e.map(smuggler, scope_dirsearch)
+    #e.map(getjs, nothing)
+    #e.map(s3brute, scope_s3brute)
+#[+]----------------------------------------------------------------------------[+]
+#get output from dirsearch folder and from smuggler folder
+#smuggler output
+for i in scope_wappalyzer:
+    i = i.replace("://", "_")
+    i = i.replace(".", "_")
+    x = subprocess.check_output("ls webtool/smuggler/payloads/", stderr=subprocess.STDOUT, shell=True)
+    find = re.findall(i + "(.*)\\\\n", str(x))
+    try:
+        os.system("mv webtool/smuggler/payloads/" + i + find[0] + " " + args.o + "/other/smuggler/")
+    except:
+        pass
+#dirsearch output
+for i in scope_wappalyzer:
+    i = i.replace("http://", "")
+    i = i.replace("https://", "")
+    try:
+        os.system("cat webtool/dirsearch/reports/*/* | grep " + i + "| sort -u >> " + args.o + "/other/dirsearch/dirsearch")
+    except:
+        pass
+# functions needed in next concurrent.futures
+def brutespray(nothing):
+    os.system("python3 webtool/brutespray/brutespray.py -f " + args.o + "/network/nmap.xml -o " + args.o + "/network/brutespray --threads 5 --hosts 5")
+
+def nuclei_scan(nothing):
+    os.system("nuclei -t ~/nuclei-templates/cves/ -l " + args.o + "/recon/subdomains/httpx_subs -c 50 -o " + args.o + "/nuclei/cves/cves")
+    os.system("nuclei -t ~/nuclei-templates/exposed-panels/ -l " + args.o + "/recon/subdomains/httpx_subs -c 50 -o " + args.o + "/nuclei/exposed-panels/exposed-panels")
+    os.system("nuclei -t ~/nuclei-templates/exposures/ -l " + args.o + "/recon/subdomains/httpx_subs -c 50 -o " + args.o + "/nuclei/exposures/exposures")
+    #os.system("nuclei -t ~/nuclei-templates/exposed-tokens/ -l " + args.o + "/recon/subdomains/httpx_subs -c 50 -o " + args.o + "/nuclei/exposed-tokens/exposed-tokens")
+    os.system("nuclei -t ~/nuclei-templates/misconfiguration/ -l " + args.o + "/recon/subdomains/httpx_subs -c 50 -o " + args.o + "/nuclei/misconfiguration/misconfiguration")
+    os.system("nuclei -t ~/nuclei-templates/network/ -l " + args.o + "/recon/subdomains/httpx_subs -c 50 -o " + args.o + "/nuclei/network/network")
+    os.system("nuclei -t ~/nuclei-templates/vulnerabilities/ -l " + args.o + "/recon/subdomains/httpx_subs -c 50 -o " + args.o + "/nuclei/vulnerabilities/vulnerabilities")
+    os.system("nuclei -t ~/nuclei-templates/miscellaneous/phpmyadmin-setup.yaml -tags misc -l " + args.o + "/recon/subdomains/httpx_subs -bs 50 -o " + args.o + "/nuclei/miscellaneous/miscellaneous")
+
+def jaeles_scan(nothing):
+    os.system("jaeles scan -c 50 -s ~/.jaeles/base-signatures/cves/ -U " + args.o + "/recon/subdomains/httpx_subs -o " + args.o + "/jaeles/cves")
+    os.system("jaeles scan -c 50 -s ~/.jaeles/base-signatures/common/ -U " + args.o + "/recon/subdomains/httpx_subs -o " + args.o + "/jaeles/common")
+    os.system("jaeles scan -c 50 -s ~/.jaeles/base-signatures/sensitive/ -U " + args.o + "/recon/subdomains/httpx_subs -o " + args.o + "/jaeles/sensitive")
+
+def jaeles_fuzz(nothing):
+    #os.system("jaeles scan -c 100 -s ~/.jaeles/base-signatures/fuzz/cors/cors-bypass.yaml -H 'Cookie: " + args.c + "' -U " + args.o + "/recon/all_endpoints_and_gf/gf_parameters/cors -o " + args.o + "/jaeles/fuzz")
+    os.system("jaeles scan -c 100 -s ~/.jaeles/base-signatures/fuzz/common/CRLF.yaml -U " + args.o + "/recon/all_endpoints_and_gf/gf_parameters/parms_endpoints -o " + args.o + "/jaeles/fuzz")
+    os.system("jaeles scan -c 100 -s ~/.jaeles/base-signatures/fuzz/sqli/sqli-polyglot.yaml -U " + args.o + "/recon/all_endpoints_and_gf/gf_parameters/sqli -o " + args.o + "/jaeles/fuzz -v")
+    os.system("jaeles scan -c 100 -s ~/.jaeles/base-signatures/fuzz/sqli/sqli-polyglot-path.yaml -U " + args.o + "/recon/all_endpoints_and_gf/gf_parameters/parms_endpoints -o " + args.o + "/jaeles/fuzz")
+    os.system("jaeles scan -c 100 -s ~/.jaeles/base-signatures/fuzz/lfi/lfi-path-01.yaml -U " + args.o + "/recon/all_endpoints_and_gf/gf_parameters/parms_endpoints -o " + args.o + "/jaeles/fuzz")
+    os.system("jaeles scan -c 100 -s ~/.jaeles/base-signatures/fuzz/lfi/lfi-param-base.yaml -U " + args.o + "/recon/all_endpoints_and_gf/gf_parameters/path_traversal -o " + args.o + "/jaeles/fuzz")
+    os.system("jaeles scan -c 100 -s ~/.jaeles/base-signatures/fuzz/ssti/template-injection.yaml -U " + args.o + "/recon/all_endpoints_and_gf/gf_parameters/ssti -o " + args.o + "/jaeles/fuzz")
+    os.system("jaeles scan -c 100 -s ~/.jaeles/base-signatures/fuzz/common/ssrf.yaml -p 'dest=" + args.ct + "' -U " + args.o + "/recon/all_endpoints_and_gf/gf_parameters/ssrf -o " + args.o + "/jaeles/fuzz")
+    os.system("jaeles scan -c 100 -s ~/.jaeles/base-signatures/fuzz/common/OS_command_injection.yaml -p 'ssrf=" + args.ct + "' -U " + args.o + "/recon/all_endpoints_and_gf/gf_parameters/rce -o " + args.o + "/jaeles/fuzz")
+    os.system("jaeles scan -c 100 -s ~/.jaeles/base-signatures/fuzz/random/common-error.yaml -U " + args.o + "/recon/all_endpoints_and_gf/gf_parameters/parms_endpoints -o " + args.o + "/jaeles/fuzz")
+    os.system("jaeles scan -c 100 -s ~/.jaeles/base-signatures/fuzz/open-redirect/open-redirect-lite.yaml -U " + args.o + "/recon/all_endpoints_and_gf/gf_parameters/parms_endpoints -o " + args.o + "/jaeles/fuzz")
+    os.system("jaeles scan -c 100 -s ~/.jaeles/base-signatures/fuzz/open-redirect/open-redirect-param-base.yaml -U " + args.o + "/recon/all_endpoints_and_gf/gf_parameters/redirect -o " + args.o + "/jaeles/fuzz")
+    os.system("jaeles scan -c 100 -s ~/.jaeles/base-signatures/fuzz/common/host-header-injection.yaml -p 'dest=test.com' -U " + args.o + "/recon/subdomains/httpx_subs -o " + args.o + "/jaeles/fuzz")
+
+def nuclei_fuzz(nothing):
+    os.system("nuclei -t ~/nuclei-templates/vulnerabilities/generic/error-based-sql-injection.yaml -tags fuzz -l " + args.o + "/recon/all_endpoints_and_gf/gf_parameters/sqli -bs   50 -o " + args.o + "/nuclei/fuzz/error-based-sql-injection")
+    os.system("nuclei -t ~/nuclei-templates/vulnerabilities/generic/crlf-injection.yaml -l " + args.o + "/recon/all_endpoints_and_gf/gf_parameters/parms_endpoints -bs 50 -o " + args.o + "/nuclei/fuzz/crlf-injection")
+    os.system("nuclei -t ~/nuclei-templates/vulnerabilities/generic/open-redirect.yaml -l " + args.o + "/recon/all_endpoints_and_gf/gf_parameters/parms_endpoints -bs 50 -o " + args.o + "/nuclei/fuzz/open-redirect")
+    os.system("nuclei -t ~/nuclei-templates/nuclei-templates/fuzzing/adminer-panel-fuzz.yaml -tags fuzz -l " + args.o + "/recon/subdomains/httpx_subs -bs 50 -o " + args.o + "/nuclei/fuzz/adminer-panel-fuzz")
+    os.system("nuclei -t ~/nuclei-templates/vulnerabilities/generic/top-xss-params-omar.yaml -l " + args.o + "/recon/all_endpoints_and_gf/gf_parameters/parms_endpoints -bs 50 -o " + args.o + "/nuclei/fuzz/top-xss-params")
+    os.system("nuclei -t ~/nuclei-templates/vulnerabilities/generic/basic-xss-prober.yaml -l " + args.o + "/recon/subdomains/httpx_subs -bs 50 -o " + args.o + "/nuclei/fuzz/top-xss-params")
+    os.system("nuclei -t ~/nuclei-templates/headless/postmessage-tracker.yaml -headless -tags headless -l " + args.o + "/recon/all_endpoints_and_gf/gf_parameters/parms_endpoints -bs 100 -o " + args.o + "/nuclei/fuzz/postmessage-tracker")
+
+def dalfox(nothing):
+    os.system("mkdir " + args.o + "/other/Gxss-dalfox")
+    os.system("cat " + args.o + "/recon/all_endpoints_and_gf/gf_parameters/Gxss-dalfox |Gxss -c 100|qsreplace -a|tee -a " + args.o + "/other/Gxss-dalfox/Gxss")
+    if args.c is not None:
+        os.system("cat " + args.o + "/other/Gxss-dalfox/Gxss |qsreplace -a| dalfox pipe -b https://omar0x01.xss.ht -C " + args.c + " -o " + args.o + "/other/Gxss-dalfox/dalfox")
+    else:
+        os.system("cat " + args.o + "/other/Gxss-dalfox/Gxss |qsreplace -a| dalfox pipe -b https://omar0x01.xss.ht -o " + args.o + "/other/Gxss-dalfox/dalfox")
+
+#[+]----------------------------------------------------------------------------[+]
+#make needing directories and params
+#gf
+os.system("mkdir " + args.o + "/recon/all_endpoints_and_gf/gf_parameters")
+os.system("cat " + args.o + "/recon/all_endpoints_and_gf/endpoints|grep = |qsreplace -a > " + args.o + "/recon/all_endpoints_and_gf/gf_parameters/parms_endpoints")
+os.system("cat " + args.o + "/recon/all_endpoints_and_gf/endpoints|grep = |qsreplace -a|gf sqli > " + args.o + "/recon/all_endpoints_and_gf/gf_parameters/sqli")
+os.system("cat " + args.o + "/recon/all_endpoints_and_gf/endpoints|grep = |qsreplace -a|gf ssrf > " + args.o + "/recon/all_endpoints_and_gf/gf_parameters/ssrf")
+os.system("cat " + args.o + "/recon/all_endpoints_and_gf/endpoints|grep = |qsreplace -a|gf ssti > " + args.o + "/recon/all_endpoints_and_gf/gf_parameters/ssti")
+os.system("cat " + args.o + "/recon/all_endpoints_and_gf/endpoints|grep = |qsreplace -a|gf lfi > " + args.o + "/recon/all_endpoints_and_gf/gf_parameters/path_traversal")
+os.system("cat " + args.o + "/recon/all_endpoints_and_gf/endpoints|grep = |qsreplace -a|gf img-traversal >> " + args.o + "/recon/all_endpoints_and_gf/gf_parameters/path_traversal")
+os.system("cat " + args.o + "/recon/all_endpoints_and_gf/endpoints|grep = |qsreplace -a|gf idor > " + args.o + "/recon/all_endpoints_and_gf/gf_parameters/idor")
+os.system("cat " + args.o + "/recon/all_endpoints_and_gf/endpoints|grep = |qsreplace -a|gf debug_logic > " + args.o + "/recon/all_endpoints_and_gf/gf_parameters/logic")
+os.system("cat " + args.o + "/recon/all_endpoints_and_gf/endpoints|grep = |qsreplace -a|gf rce > " + args.o + "/recon/all_endpoints_and_gf/gf_parameters/rce")
+os.system("cat " + args.o + "/recon/subdomains/live_subdomains|grep = |qsreplace -a|gf interestingsubs > " + args.o + "/recon/all_endpoints_and_gf/gf_parameters/interesting_subs")
+os.system("cat " + args.o + "/recon/all_endpoints_and_gf/endpoints|grep = |qsreplace -a|gf redirect > " + args.o + "/recon/all_endpoints_and_gf/gf_parameters/redirect")
+os.system("cat " + args.o + "/recon/all_endpoints_and_gf/endpoints|egrep -iv '\.(jpg|jpeg|gif|css|tif|tiff|png|ttf|woff|woff2|ico|pdf|svg|txt|js)'|grep = |qsreplace -a > " + args.o + "/recon/all_endpoints_and_gf/gf_parameters/Gxss-dalfox")
+
+#make directories for nuclei
+os.system("mkdir " + args.o + "/nuclei")
+os.system("mkdir " + args.o + "/nuclei/cves")
+os.system("mkdir " + args.o + "/nuclei/exposed-panels")
+os.system("mkdir " + args.o + "/nuclei/exposed-tokens")
+os.system("mkdir " + args.o + "/nuclei/exposures")
+os.system("mkdir " + args.o + "/nuclei/vulnerabilities")
+os.system("mkdir " + args.o + "/nuclei/network")
+os.system("mkdir " + args.o + "/nuclei/misconfiguration")
+os.system("mkdir " + args.o + "/nuclei/miscellaneous")
+os.system("mkdir " + args.o + "/nuclei/fuzz")
+os.system("mkdir " + args.o + "/nuclei/headless")
+#make directories for jaeles
+os.system("mkdir " + args.o + "/jaeles")
+os.system("mkdir " + args.o + "/jaeles/common")
+os.system("mkdir " + args.o + "/jaeles/cves")
+os.system("mkdir " + args.o + "/jaeles/sensitive")
+os.system("mkdir " + args.o + "/jaeles/fuzz")
+with concurrent.futures.ThreadPoolExecutor(max_workers=10) as e:
+    # start brutespray
+    nothing = [1]
+    os.system("nuclei -ut")
+    #e.map(brutespray, nothing)
+    e.map(nuclei_scan, nothing)
+    e.map(jaeles_scan, nothing)
+    e.map(jaeles_fuzz, nothing)
+    e.map(nuclei_fuzz, nothing)
+    #e.map(dalfox, nothing)
